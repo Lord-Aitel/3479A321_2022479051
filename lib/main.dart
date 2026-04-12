66f97cbaf1faa93935f5037800dfcc77e4b44dfd
Matias Peters <matiaspeters77@gmail.com>
2026-04-11 23:57:45 -0400

    Integración de logger en main.dart para debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+var logger = Logger();
 
 void main() {
+  logger.d('Iniciando la aplicación de Buscaminas'); // Debug
+  logger.i('Iniciando la aplicación de Buscaminas'); // Info
+  logger.w('Iniciando la aplicación de Buscaminas'); // Warning
+  logger.e('Iniciando la aplicación de Buscaminas'); // Error
+
   runApp(const MyApp());
 }
 
@@ -13,21 +21,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
