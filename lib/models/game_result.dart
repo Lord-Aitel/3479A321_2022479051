0ee17d623e53088e845279077334212009b63122
Matias Peters <matiaspeters77@gmail.com>
2026-04-22 18:04:13 -0400

    integracion de historial junto con partidas de prueba modelo gameresult y listview

---
+++ lib/models/game_result.dart
@@ -0,0 +1,12 @@
+class GameResult {
+  
+  final String date;
+  final String timeSpent;
+  final bool isVictory;
+
+  GameResult({
+    required this.date,
+    required this.timeSpent,
+    required this.isVictory
+  });
+}
--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
+import '/models/game_result.dart';
+
+
+
 class HistoryScreen extends StatelessWidget {
 const HistoryScreen({super.key});
 @override
 Widget build(BuildContext context) {
- return Scaffold(
- appBar: AppBar(title: const Text('Historial (Maqueta)')),
- body: const Center(
- child: Text('Aquí irá la lista de partidas.'),
- ),
- );
-}
+ final List<GameResult> history = [
+  GameResult(date: "22/04/2026", timeSpent: "2m 30s", isVictory: true),
+  GameResult(date: "21/04/2026", timeSpent: "1m 45s", isVictory: false),
+ ];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Historial de Partidas")),
+      body: ListView.builder(
+        itemCount: history.length,
+        itemBuilder: (context, index) {
+          final game = history[index];
+          return ListTile(
+            title: Text("Fecha: ${game.date}"),
+            subtitle: Text("Duración: ${game.timeSpent}"),
+            trailing: Icon(
+              game.isVictory ? Icons.check_circle : Icons.cancel,
+              color: game.isVictory ? Colors.green : Colors.red,
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
